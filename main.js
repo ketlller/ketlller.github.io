1cc4909202e2cebf880bd8301276df6ddb257f90
ketlller <oculusvrdk2@gmail.com>
2020-02-15 04:33:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ var divSellCOIN_USD_hotbit = document.querySelector('.sellCOIN_USD_hotbit span')
 var divBuyCOIN_USD_livecoin1 = document.querySelector('.BuyCOIN_USD_livecoin1 span');
 var divSellCOIN_USD_livecoin1 = document.querySelector('.SellCOIN_USD_livecoin1 span');
 var divBuyCOIN_USD1 = document.querySelector('.BuyCOIN_USD1 span');
-var divSellCOIN_USD11 = document.querySelector('.SellCOIN_USD11 span');
+var divSellCOIN_USD1 = document.querySelector('.SellCOIN_USD1 span');
 var divBuyCOIN_USD_hotbit1 = document.querySelector('.BuyCOIN_USD_hotbit1 span');
 var divSellCOIN_USD_hotbit1 = document.querySelector('.SellCOIN_USD_hotbit1 span');
  
@@ -156,7 +156,7 @@ console.log()
     .then(res => {
       res.json().then(body => {
       divBuyCOIN_USD.innerHTML = body.buy[0].price;  
-    divBuyCOIN_USD1.innerHTML = body.buy[0].amount; 
+    //divBuyCOIN_USD1.innerHTML = body.buy[0].amount; 
       divSellCOIN_USD.innerHTML = body.sell[0].price;
     //divSellCOIN_USD1.innerHTML = body.sell[0].amount;
     
@@ -200,9 +200,11 @@ console.log()
       hotbitbtcalpha.innerHTML = value5; 	      
       
 	      
-var value16 = (((currentPrice_SellCOIN_USD_livecoin*100)/currentPrice_SellCOIN_USD)-100).toFixed(2); 	 
-      divSellCOIN_USD11.innerHTML = value16;	      
-	console.log(body);      
+var value10 = (((currentPrice_SellCOIN_USD_livecoin*100)/currentPrice_SellCOIN_USD)-100).toFixed(2); 	 
+      divSellCOIN_USD1.innerHTML = value10;	      
+	console.log(body);     
+var value11 = (((currentPrice_BuyCOIN_USD_hotbit*100)/currentPrice_SellCOIN_USD)-100).toFixed(2);         
+      divBuyCOIN_USD1.innerHTML = value11;	      
         
        
       
